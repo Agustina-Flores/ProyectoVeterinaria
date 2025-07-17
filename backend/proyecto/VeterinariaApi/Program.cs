a9cf257fa3bd88ea51eedd28fdbb7b32e7526691
Agustina <agustina.flores@wirsolut.com>
2025-07-17 14:04:37 -0300

    despliegue angular

--- backend/proyecto/VeterinariaApi/Program.cs
+++ backend/proyecto/VeterinariaApi/Program.cs
@@ -40,7 +40,9 @@ builder.Services.AddAuthorization();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:4200",
+        policy.WithOrigins(
+        "http://localhost:4200",
+        "http://localhost:8080",
          "https://proyectoveterinaria.onrender.com",
         "https://veterinaria-frontend-74k3.onrender.com")
               .AllowAnyHeader()
